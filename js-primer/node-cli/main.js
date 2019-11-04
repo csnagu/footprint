ce10901027ae95264e4e01bdc6f01c00e6f1b64f
Chikara Saito <nagulog@gmail.com>
2019-11-04 20:02:29 +0900

    FIN ユニットテストを記述する

---
+++ js-primer/node-cli/main.js
@@ -0,0 +1,33 @@
+const program = require("commander");
+const fs = require("fs");
+const md2html = require("./md2html.js");
+
+// オプションを定義する
+program.option("--gfm", "GFMを有効にする");
+program.option("--debug", "デバッグモードを有効にする");
+
+// コマンドライン引数からファイルパスを取得する
+program.parse(process.argv);
+const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+    gfm: false,
+    debug: false,
+    ...program.opts()
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1);
+        return;
+    }
+
+    if (cliOptions.debug) {
+        console.log("Debug mode........");
+    }
+    // MarkdownファイルをHTML文字列に変換する
+    const html = md2html(file, cliOptions);
+    console.log(html);
+})
