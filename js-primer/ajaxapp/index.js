fe563ec4b887ad8c4309b381e71462c43bb928f2
Chikara Saito <nagulog@gmail.com>
2019-11-10 20:05:57 +0900

    FIN Ajaxで通信を修了

--- js-primer/ajaxapp/index.js
+++ js-primer/ajaxapp/index.js
@@ -1,26 +1,64 @@
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch(error) {
+        console.error(`エラーが発生しました (${error})`);
+    };
+}
+
 function fetchUserInfo(userId) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
-            console.log(response.status);
-            if (!response.ok) {
-                console.error("サーバーエラー", response);
+             if (!response.ok) {
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
             } else {
-                response.json().then(userInfo => {
-                    const view = `
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd>${userInfo.public_repos}</dd>
-                    </dl>
-                    `;
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
-                });
+                return response.json();
             }
-        }).catch(error => {
-            console.error("ネットワークエラー", error);
         });
+}
+
+function getUserId() {
+    const userId = document.getElementById("userId").value;
+    return encodeURIComponent(userId);
+}
+
+function createView(userInfo) {
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+    const result = document.getElementById("result");
+    result.innerHTML = view;
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i - 1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
 }
