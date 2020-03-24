1cad1e5fcac26f1610f0d56469c3625fcd96f0f3
Chikara Saito <s.chikara3110@gmail.com>
2020-03-24 23:30:28 +0900

    WIP 1sho-jin

---
+++ atcoder/abc139a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S, T;
+    int ans = 0;
+    cin >> S >> T;
+    for (int i = 0; i < 3; i++)
+    {
+        if (S[i] == T[i])
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
