761f87637d96575840b085e107398529d4715427
Chikara Saito <s.chikara3110@gmail.com>
2020-04-05 13:35:12 +0900

    WIP 1sho-jin

--- atcoder/abc056c.cpp
+++ atcoder/abc056c.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
@@ -7,13 +6,14 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int K, X;
-    cin >> K >> X;
-    K--;
-    for (int i = X - K; i <= X + K; i++)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
+    long long X;
+    cin >> X;
+    int pos = 0;
+    int i;
+    for (i = 0; pos < X; i++)
+        pos += i;
+
+    cout << --i << endl;
+
     return 0;
 }
